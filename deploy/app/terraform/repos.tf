b787cb95b7b429e793e55feeebf8f82af71966ef
Norman Aberin <norman.japheth.aberin@oracle.com>
2022-01-27 11:50:53 -0600

    Uncommented repository creation

--- deploy/app/terraform/repos.tf
+++ deploy/app/terraform/repos.tf
@@ -1,15 +1,9 @@
 //------- Create repos End  ------------------------------------------
-/*
+
 resource "oci_artifacts_container_repository" "frontend_helidon_container_repository" {
   #Required
   compartment_id = "${var.ociCompartmentOcid}"
   display_name = "${var.runName}/frontend-helidon"
-  is_public = true
-}
-resource "oci_artifacts_container_repository" "helidonatp_container_repository" {
-  #Required
-  compartment_id = "${var.ociCompartmentOcid}"
-  display_name = "${var.runName}/admin-helidon"
   is_public = true
 }
 resource "oci_artifacts_container_repository" "order-helidon_container_repository" {
@@ -30,22 +24,3 @@ resource "oci_artifacts_container_repository" "inventory-helidon_container_repos
   display_name = "${var.runName}/inventory-helidon"
   is_public = true
 }
-resource "oci_artifacts_container_repository" "inventory-python_container_repository" {
-  #Required
-  compartment_id = "${var.ociCompartmentOcid}"
-  display_name = "${var.runName}/inventory-python"
-  is_public = true
-}
-resource "oci_artifacts_container_repository" "nodejs_container_repository" {
-  #Required
-  compartment_id = "${var.ociCompartmentOcid}"
-  display_name = "${var.runName}/inventory-nodejs"
-  is_public = true
-}
-resource "oci_artifacts_container_repository" "inventory-helidon-se_container_repository" {
-  #Required
-  compartment_id = "${var.ociCompartmentOcid}"
-  display_name = "${var.runName}/inventory-helidon-se"
-  is_public = true
-}
-*/
