2dd9913ed4f2fb6cfdfab37bcf51f52b9da40d0d
Norman Aberin <aberinnj@gmail.com>
2022-02-03 11:41:09 -0600

    Added simple app-react-test base files

---
+++ app-react-test/src/App.js
@@ -0,0 +1,17 @@
+import logo from './oracle.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Example React application
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
