40fba5fd6e17e09e947e9d65c575c717cc4bdf7a
Norman Aberin <aberinnj@gmail.com>
2022-02-08 16:35:32 -0600

    Updated App p value

--- app-react-test/src/App.js
+++ app-react-test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Output
+          Hello World!
         </p>
       </header>
     </div>
