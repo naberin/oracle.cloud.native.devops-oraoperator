d709096db6c975284a7898909194e5cbcf7627c2
naberin <aberinnj@gmail.com>
2022-03-11 16:17:48 -0600

    Removed old infrastructure terraform scripts

--- modules/infrastructure/app/main_var.tf
+++
@@ -1,8 +0,0 @@
-//Copyright (c) 2021 Oracle and/or its affiliates.
-//Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-variable "tenancy_ocid" {}
-variable "current_user_ocid" {}
-variable "compartment_ocid" {}
-variable "region" {}
-
-variable "repository" { default="oracicd" }
--- modules/infrastructure/app/repos.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_artifacts_container_repository" "frontend_helidon_container_repository" {
-  #Required
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "${var.repository}/frontend-react"
-  is_public = true
-}
