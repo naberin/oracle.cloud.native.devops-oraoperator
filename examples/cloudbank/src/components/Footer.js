b94693030cae0006ee5d4996d328416630c86ece
naberin <aberinnj@gmail.com>
2022-03-24 15:46:31 -0500

    Updated Footer

--- examples/cloudbank/src/components/Footer.js
+++ examples/cloudbank/src/components/Footer.js
@@ -3,11 +3,11 @@ const Component = function () {
     return (
         <footer className={"app-footer"}>
             <div className={"footer-links flex flex-row flex-justify-center"}>
-                <div className={"footer-link"}>Contact Us</div>
-                <div className={"footer-link"}>Privacy</div>
+                <div className={"footer-link link"}>Contact Us</div>
+                <div className={"footer-link link"}>Privacy</div>
 
-                <div className={"footer-link"}>Security</div>
-                <div className={"footer-link"}>About CloudBank</div>
+                <div className={"footer-link link"}>Security</div>
+                <div className={"footer-link link"}>About CloudBank</div>
             </div>
             <div className="copyright">© Oracle Corporation</div>
         </footer>
