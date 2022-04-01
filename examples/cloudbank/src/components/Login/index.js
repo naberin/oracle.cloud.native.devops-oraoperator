33c764708fb1ae31434b14ddea6cfb2e0a56dfa3
naberin <aberinnj@gmail.com>
2022-04-01 16:56:46 -0500

    Updated Login component structure

--- examples/cloudbank/src/components/Login/index.js
+++ examples/cloudbank/src/components/Login/index.js
@@ -5,7 +5,7 @@ import LoginModal from "./modal";
 class Component extends React.Component {
     render() {
         return (
-            <section className="login app-section flex-grow-8">
+            <section className="login app-section flex-grow-10">
                 <LoginModal/>
             </section>
         )
