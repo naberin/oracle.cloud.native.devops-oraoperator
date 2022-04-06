924a43ed63993d84fd13e26d73efda8695f0d721
Norman Aberin <55222849+naberin@users.noreply.github.com>
2022-04-05 20:47:10 -0500

    Cloudbank starter (#31)
    * Added root styles
    
    * Added CSS Styling and Header
    
    * Added footer and improved flex styling
    
    * Updated Footer
    
    * Added Login page
    
    * Updated CSS styling
    
    * Updated Login component structure
    
    * Added Transfer page, nav, form and request
    
    * Updated buttons with feedback to depress
    
    * Added images and styling
    
    * Updated transfer with new useEffect, loading sources and destinations
    
    * Updated routing and navigation with history
    
    * Added Dockerfile
    
    * Updated endpoints
    
    * Updated routing temporarily
    
    * Added authentication guards and page protection
    
    * Updated styling to buttons
    
    * Added simple .env
    
    * Added manifest.yaml

--- examples/cloudbank/src/index.js
+++ examples/cloudbank/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path="/*" element={<App/>} />
+      </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
