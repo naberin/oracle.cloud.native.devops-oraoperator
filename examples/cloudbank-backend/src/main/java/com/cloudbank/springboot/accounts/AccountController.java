9c17437cb10d8693b464ee3fba21f44df87ffdde
naberin <aberinnj@gmail.com>
2022-04-12 15:49:50 -0500

    Added CrossOrigin setting

--- examples/cloudbank-backend/src/main/java/com/cloudbank/springboot/accounts/AccountController.java
+++ examples/cloudbank-backend/src/main/java/com/cloudbank/springboot/accounts/AccountController.java
@@ -1,6 +1,7 @@
 package com.cloudbank.springboot.accounts;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/account")
 public class AccountController {
 
