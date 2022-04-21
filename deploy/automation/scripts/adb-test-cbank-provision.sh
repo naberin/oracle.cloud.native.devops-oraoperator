f02682b56fd8896fc0e5f7665bdfb9044cbb046d
naberin <aberinnj@gmail.com>
2022-04-21 08:36:24 -0500

    Added automation scripts and templates

--- deploy/automation/scripts/adb-test-cbank-provision.sh
+++ deploy/automation/scripts/adb-test-cbank-provision.sh
@@ -3,18 +3,18 @@
 # #########
 # Variables
 # #########
-ADBNAME=$1
+DBNAME=$1
 OCICOMPARTMENTOCID=$2
 OCIVAULTOCID=$3
 ADBRESOURCENAME='CLOUDBANKDB'
 
 # ##########################################
-# Provision
+# ProvisionWallet_cloudbankDB.zip
 # provisions the database using the Operator
 # ##########################################
 
-if [ ! -z "$ADBNAME" ];
+if [ ! -z "$DBNAME" ];
 then
-    tmplt=`cat "../templates/adb-provision.yml.template" | sed -e "s/{{ADBRESOURCENAME}}/$ADBRESOURCENAME/g" -e "s/{{ADBNAME}}/$ADBNAME/g" -e "s/{{OCICOMPARTMENTOCID}}/$OCICOMPARTMENTOCID/g" -e "s/{{OCIVAULTOCID}}/$OCIVAULTOCID/g"`
+    tmplt=`cat "../templates/adb-provision.yml.template" | sed -e "s/{{ADBRESOURCENAME}}/$ADBRESOURCENAME/g" -e "s/{{ADBNAME}}/$DBNAME/g" -e "s/{{OCICOMPARTMENTOCID}}/$OCICOMPARTMENTOCID/g" -e "s/{{OCIVAULTOCID}}/$OCIVAULTOCID/g"`
     echo "$tmplt" | kubectl apply -f -;
 fi
---
+++ deploy/automation/scripts/adb-test-cbank-recreate.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+# #########
+# Variables
+# #########
+DBNAME=$1
+DBCONNECTIONOCID=$2
+DBSCRIPTLOCATION=$3
+
+
+# #############################################
+# Recreate
+# Recreates the expected database objects
+# such as the CloudBank schema and test data
+# using sqlcl
+# #############################################
+env {
+    echo "oci profile DEFAULT"
+    echo "conn ${DBCONNECTIONOCID}"
+    echo "script ${DBSCRIPTLOCATION}"
+    echo "exit"
+} | sql /nolog
+
+
+####
+# 
+####
