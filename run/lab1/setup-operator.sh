660c439c23d0af40ac20b1b986f18bb133c318ff
naberin <aberinnj@gmail.com>
2022-04-22 13:17:52 -0500

    Increased wait time inside setup-operator script

--- run/lab1/setup-operator.sh
+++ run/lab1/setup-operator.sh
@@ -4,13 +4,17 @@
 # The operator uses webhooks for validating user input before persisting it in Etcd. 
 # Webhooks require TLS certificates that are generated and managed by a certificate 
 # manager.
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
+echo 'Installing cert-manager...';
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml;
 
 
 # wait
-# todo: observe the right length of time for waiting
-sleep 10s
+echo '';
+echo 'Waiting for cert-manager...';
+sleep 30s;
 
 # install the operator
 # Operator pod replicas are set to a default of 3 for High Availability, which can be scaled up and down.
-kubectl apply -f https://raw.githubusercontent.com/oracle/oracle-database-operator/main/oracle-database-operator.yaml
+echo '';
+echo 'Installing the Oracle Database Operator...';
+kubectl apply -f https://raw.githubusercontent.com/oracle/oracle-database-operator/main/oracle-database-operator.yaml;
