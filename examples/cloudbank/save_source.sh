cb511a98ef314d38adef01a63c05adaec9cecfd2
naberin <aberinnj@gmail.com>
2022-06-16 11:19:17 -0500

    Fixed save_source

--- examples/cloudbank/save_source.sh
+++ examples/cloudbank/save_source.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 # expects state_functions.env to exist inside CB_STATE_DIR
 # expects environment variables set by source.env or ~/.bashrc
-echo """
 
-# ====================== BEGIN CLOUDBANK SOURCE ENV ======================
 
-export LAB_HOME=${LAB_HOME}
-export ROOT_DIR=${ROOT_DIR}
-export CB_ROOT_DIR=${CB_ROOT_DIR}
-export CB_STATE_DIR=${CB_STATE_DIR}
-export CB_TERRAFORM_DIR=${CB_TERRAFORM_DIR}
-export CB_KUBERNETES_TEMPLATES_DIR=${CB_KUBERNETES_TEMPLATES_DIR}
-export CB_KUBERNETES_GEN_FILES_DIR=${CB_KUBERNETES_GEN_FILES_DIR}
 
-source ${CB_STATE_DIR}/state_functions.env
-# ====================== END CLOUDBANK SOURCE ENV ==========================
-""" >> ~/.zshrc
+if [ -f ~/.bashrc ]; then
+
+  echo """
+  # ====================== BEGIN CLOUDBANK SOURCE ENV ======================
+
+  export LAB_HOME=${LAB_HOME}
+  export ROOT_DIR=${ROOT_DIR}
+  export CB_ROOT_DIR=${CB_ROOT_DIR}
+  export CB_STATE_DIR=${CB_STATE_DIR}
+  export CB_TERRAFORM_DIR=${CB_TERRAFORM_DIR}
+  export CB_KUBERNETES_TEMPLATES_DIR=${CB_KUBERNETES_TEMPLATES_DIR}
+  export CB_KUBERNETES_GEN_FILES_DIR=${CB_KUBERNETES_GEN_FILES_DIR}
+
+  source ${CB_STATE_DIR}/state_functions.env
+  # ====================== END CLOUDBANK SOURCE ENV ==========================
+  """ >> ~/.bashrc
+
+
+elif [ -f ~/.zshrc ]; then
+
+  echo """
+  # ====================== BEGIN CLOUDBANK SOURCE ENV ======================
+
+  export LAB_HOME=${LAB_HOME}
+  export ROOT_DIR=${ROOT_DIR}
+  export CB_ROOT_DIR=${CB_ROOT_DIR}
+  export CB_STATE_DIR=${CB_STATE_DIR}
+  export CB_TERRAFORM_DIR=${CB_TERRAFORM_DIR}
+  export CB_KUBERNETES_TEMPLATES_DIR=${CB_KUBERNETES_TEMPLATES_DIR}
+  export CB_KUBERNETES_GEN_FILES_DIR=${CB_KUBERNETES_GEN_FILES_DIR}
+
+  source ${CB_STATE_DIR}/state_functions.env
+  # ====================== END CLOUDBANK SOURCE ENV ==========================
+  """ >> ~/.zshrc
+
+fi
