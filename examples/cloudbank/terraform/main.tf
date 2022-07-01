5e5cb0f0db7f1a972a9141add27aebb0ac23c878
naberin <aberinnj@gmail.com>
2022-07-01 17:22:15 -0500

    Updated main.tf module

--- examples/cloudbank/terraform/main.tf
+++ examples/cloudbank/terraform/main.tf
@@ -1,8 +1,8 @@
 
-module distributed-builds-private-jenkins {
+module jenkins-build {
   source = "./jenkins-module"
   compartment_id     = var.compartment_ocid
   jenkins_password   = var.jenkins_password
   region             = var.region
-  unique_agent_names = ["agent1"] # Only one agent
+#  unique_agent_names = ["agent1"] # Only one agent
 }
