40b5ff8f61cd951adf7e1a51539e06a9831f2719
Norman Aberin <55222849+naberin@users.noreply.github.com>
2022-07-15 13:25:57 -0500

    CBTRDEV09-fixingjenkinsfile (#33)
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Updated generate SIDB scripts and Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Added fix
    
    * Update Jenkinsfile
    
    * Jenkinsfile
    
    * Update Jenkinsfile
    
    * Updated Jenkinsfile

--- examples/cloudbank/scripts/gen-sidb-create.sh
+++ examples/cloudbank/scripts/gen-sidb-create.sh
@@ -1,12 +1,16 @@
 # initialization
 STATE_LOCATION=$CB_STATE_DIR/state.json
 
-
 # generate YAML
 echo -n "Generating YAML file..."
+SRC_FILE=$CB_KUBERNETES_TEMPLATES_DIR/sidb-create-template.yaml
 YAML_FILE=$CB_KUBERNETES_GEN_FILES_DIR/sidb-create.yaml
-cp $CB_KUBERNETES_TEMPLATES_DIR/sidb-create-template.yaml $YAML_FILE
-echo "DONE"
+SIDB_NAME=cloudbankdb
+SIDB_NAMESPACE=cloudbank
+SIDB_PWD_SECRET=sidb-admin-secret
+
+# run task
+$CB_STATE_DIR/generate-yaml-sidb-create.sh $SRC_FILE $YAML_FILE $SIDB_NAME $SIDB_NAMESPACE $SIDB_PWD_SECRET
 
 # Output copy
 echo ""
